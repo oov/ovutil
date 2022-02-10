3ad4afaa1ee596470cd3b6af0b9235f46b3e80f5
oov <ilovevipper@gmail.com>
2022-02-10 12:14:13 +0900

    move some funcs to ovbase

--- include/ovutil/str.h
+++ include/ovutil/str.h
@@ -2,11 +2,6 @@
 
 #include "ovbase.h"
 
-NODISCARD error atoi64(struct NATIVE_STR const *const s, int64_t *const dest);
-NODISCARD error atou64(struct NATIVE_STR const *const s, uint64_t *const dest);
-NODISCARD error itoa64(int64_t const v, struct NATIVE_STR *const dest);
-NODISCARD error utoa64(uint64_t const v, struct NATIVE_STR *const dest);
-
 NODISCARD error include_trailing_path_delimiter(struct NATIVE_STR *const ws);
 NODISCARD error exclude_trailing_path_delimiter(struct NATIVE_STR *const ws);
 NODISCARD error extract_file_name(struct NATIVE_STR const *const src, size_t *const pos);
