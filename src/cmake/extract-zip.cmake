e17cbd3c66e90e56dbd1a1880c3ad11427f9491c
oov <ilovevipper@gmail.com>
2022-02-24 05:52:16 +0900

    add sjis support

--- src/cmake/extract-zip.cmake
+++ src/cmake/extract-zip.cmake
@@ -8,7 +8,15 @@ endif()
 file(REMOVE_RECURSE "${local_dir}/${dir}")
 file(MAKE_DIRECTORY "${local_dir}/${dir}")
 
-execute_process(
-  COMMAND ${CMAKE_COMMAND} -E tar xzvf ${ZIP_PATH}
-  WORKING_DIRECTORY "${local_dir}/${dir}"
-)
+if ("${charset}" STREQUAL "sjis")
+  find_program(UNZIP unzip REQUIRED CMAKE_FIND_ROOT_PATH_BOTH)
+  execute_process(
+    COMMAND ${UNZIP} -O sjis ${ZIP_PATH}
+    WORKING_DIRECTORY "${local_dir}/${dir}"
+  )
+else()
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar xzvf ${ZIP_PATH}
+    WORKING_DIRECTORY "${local_dir}/${dir}"
+  )
+endif()
