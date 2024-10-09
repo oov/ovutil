ad98af4e27414f5e58e59bcfa5d0dba5a39ec419
oov <ilovevipper@gmail.com>
2024-10-09 16:40:43 +0900

    update build scripts

--- build.bash
+++ build.bash
@@ -4,21 +4,24 @@ set -eu
 CUR_DIR="${PWD}"
 cd "$(dirname "${BASH_SOURCE:-$0}")"
 
+INSTALL_TOOLS=1
 REBUILD=0
 SKIP_TESTS=0
 CMAKE_BUILD_TYPE=Release
-ARCHS="i686 x86_64"
-CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-clang}
-CMAKE_TOOL_CHANIN_FILE=${CMAKE_TOOL_CHANIN_FILE:-}
+FORMAT_SOURCES=ON
 while [[ $# -gt 0 ]]; do
   case $1 in
+    --no-install-tools)
+      INSTALL_TOOLS=0
+      shift
+      ;;
     -d|--debug)
       CMAKE_BUILD_TYPE=Debug
       shift
       ;;
-    -a|--arch)
-      ARCHS="$2"
-      shift 2
+    --no-format)
+      FORMAT_SOURCES=OFF
+      shift
       ;;
     -r|--rebuild)
       REBUILD=1
@@ -38,37 +41,31 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-mkdir -p build/tools
-cd build
+if [ "${INSTALL_TOOLS}" -eq 1 ]; then
+  mkdir -p "build/tools"
+  if [ ! -e "${PWD}/build/tools/setup-llvm-mingw.bash" ]; then
+    curl -o "${PWD}/build/tools/setup-llvm-mingw.bash" -sOL https://raw.githubusercontent.com/oov/ovbase/3f1f33bdcdf4ca938cfc9a2f82d70973b3fbe13a/setup-llvm-mingw.bash
+  fi
+  . "${PWD}/build/tools/setup-llvm-mingw.bash" --dir "${PWD}/build/tools"
+fi
 
-for arch in $ARCHS; do
-  builddir="${PWD}/${CMAKE_BUILD_TYPE}/${arch}"
-  if [ "${REBUILD}" -eq 1 ] || [ ! -e "${builddir}-ovbase/CMakeCache.txt" ]; then
-    rm -rf "${builddir}-ovbase"
-    cmake -S ../3rd/ovbase -B "${builddir}-ovbase" --preset debug \
-      -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
-      -DFORMAT_SOURCES=OFF \
-      -DCMAKE_INSTALL_PREFIX=local \
-      -DCMAKE_C_COMPILER="${CMAKE_C_COMPILER}" \
-      -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOL_CHANIN_FILE}"
-  fi
-  cmake --build "${builddir}-ovbase"
-  if [ "${SKIP_TESTS}" -eq 0 ]; then
-    ctest --test-dir "${builddir}-ovbase" --output-on-failure --output-junit testlog.xml
-  fi
+ARCHDIR=${ARCHDIR:-`uname -m`}
+destdir="build/${PWD}/${CMAKE_BUILD_TYPE}/${ARCHDIR}"
+CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:-"${destdir}/local"}
+CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-clang}
+CMAKE_TOOL_CHANIN_FILE=${CMAKE_TOOL_CHANIN_FILE:-}
 
-  if [ "${REBUILD}" -eq 1 ] || [ ! -e "${builddir}-ovutil/CMakeCache.txt" ]; then
-    rm -rf "${builddir}-ovutil"
-    cmake -S .. -B "${builddir}-ovutil" --preset debug \
-      -DOVBASE_PATH="${builddir}-ovbase" \
-      -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
-      -DFORMAT_SOURCES=OFF \
-      -DCMAKE_INSTALL_PREFIX=local \
-      -DCMAKE_C_COMPILER="${CMAKE_C_COMPILER}" \
-      -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOL_CHANIN_FILE}"
-  fi
-  cmake --build "${builddir}-ovutil"
-  if [ "${SKIP_TESTS}" -eq 0 ]; then
-    ctest --test-dir "${builddir}-ovutil" --output-on-failure --output-junit testlog.xml
-  fi
-done
+if [ "${REBUILD}" -eq 1 ] || [ ! -e "${destdir}/CMakeCache.txt" ]; then
+  rm -rf "${destdir}"
+  cmake -S . -B "${destdir}" --preset debug \
+    -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+    -DFORMAT_SOURCES="${FORMAT_SOURCES}" \
+    -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" \
+    -DCMAKE_C_COMPILER="${CMAKE_C_COMPILER}" \
+    -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOL_CHANIN_FILE}"
+fi
+cmake --build "${destdir}"
+if [ "${SKIP_TESTS}" -eq 0 ]; then
+  ctest --test-dir "${destdir}" --output-on-failure --output-junit testlog.xml
+fi
+cmake --install "${destdir}"
